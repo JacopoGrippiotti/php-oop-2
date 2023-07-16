d3140f229403f70d2b0534b3f3ef811199563ad5
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-07-16 18:29:48 +0200

    added a new class to Category.php file named Cat

--- classes/Category.php
+++ classes/Category.php
@@ -1,7 +1,11 @@
 <?php   
-   class Dog{
-    protected $name = 'dog';
+   class Dog {
+    protected $name = 'Dog';
     protected $icon = './images/dog-29-48.png';
    }
-
+   
+   class Cat{
+     protected $name = 'Cat'
+     protected $icon = './images/cat.png '
+   }
 ?>
