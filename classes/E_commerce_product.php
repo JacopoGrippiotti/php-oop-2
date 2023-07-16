9b188ca86968738a62c4e71ed281377df6bef9ab
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-07-16 18:47:01 +0200

    added a new property in EcommerceProduct class

--- classes/E_commerce_product.php
+++ classes/E_commerce_product.php
@@ -5,14 +5,16 @@
     public $productType;
     public $price;
     public $image;
+    public $description
 
-    function__construct(String $_productName, Category $_category,String $_productType, Float $_price, String $_image){
+    function__construct(String $_productName, Category $_category,String $_productType, Float $_price, String $_image, String $_description){
 
          $this->productName = $_productName;
          $this->category = $_category;
          $this->productType = $_productType;
          $this->price = $_price;
          $this->image = $_image;
+         $this->description = $_description;
     }
 
    }
