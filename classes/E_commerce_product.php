483a40886a2af297322d36d5d6878109341db626
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-07-16 18:39:04 +0200

    added a constructor to EcommerceProduct class

--- classes/E_commerce_product.php
+++ classes/E_commerce_product.php
@@ -2,9 +2,18 @@
    class EcommerceProduct{
     public $productName;
     public $category;
-    public $product_type;
+    public $productType;
     public $price;
     public $image;
+
+    function__construct($_productName, $_category, $_productType, $_price, $_image){
+
+         $this->productName = $_productName;
+         $this->category = $_category;
+         $this->productType = $_productType;
+         $this->price = $_price;
+         $this->image = $_image;
+    }
 
    }
 
