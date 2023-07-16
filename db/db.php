84ca1049c8b98be67d34291b78230ab483d95b38
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-07-16 19:15:59 +0200

    included Category.php and E_commerce_product.php files in db.php file in db folder

---
+++ db/db.php
@@ -0,0 +1,12 @@
+<?php 
+   
+   include_once __DIR__ . '/../classes/Category.php ';
+   include_once __DIR__ . '/../classes/E_commerce_product.php';
+
+
+
+   $eCommerceProducts=[
+
+    new EcommerceProduct('bacon biscuits',new Dog,'food',14.50,'./images/okeykat-w6elADh_jww-unsplash.jpg',)
+   ]
+?>
